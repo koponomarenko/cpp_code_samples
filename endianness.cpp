b6646246759cd44edc3738d1a4f048ca7b1b86e4
Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>
2020-04-08 11:37:52 +0300

    Modify endianness.cpp
    Signed-off-by: Kostiantyn Ponomarenko <konstantin.ponomarenko@gmail.com>

--- endianness.cpp
+++ endianness.cpp
@@ -1,8 +1,13 @@
-#include <iostream>
-#include <iomanip>
-#include <cstdint>
+// Byte order, big-endian, little-endian
 
-using namespace std;
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using ::std::cout;
+using ::std::endl;
+using ::std::hex;
 
 union Int_bytes {
     int32_t value;
@@ -20,7 +25,7 @@ int main()
     cout << "a.bytes[0]: " << static_cast<int32_t>(a.bytes[0]) << endl;
 
     // #2
-    int32_t byte1 =  test_value & 0x000000ff;
+    int32_t byte1 = test_value & 0x000000ff;
     int32_t byte2 = (test_value & 0x0000ff00) >> 8;
     int32_t byte3 = (test_value & 0x00ff0000) >> 16;
     int32_t byte4 = (test_value & 0xff000000) >> 24;
@@ -28,5 +33,15 @@ int main()
     cout << "byte2: " << static_cast<int32_t>(byte2) << endl;
     cout << "byte3: " << static_cast<int32_t>(byte3) << endl;
     cout << "byte4: " << static_cast<int32_t>(byte4) << endl;
-    
+
+    // #3
+    std::vector<uint8_t> bytes;
+    bytes.push_back(test_value >> 24);
+    bytes.push_back(test_value >> 16);
+    bytes.push_back(test_value >> 8);
+    bytes.push_back(test_value);
+    for (const auto& byte : bytes) {
+        cout << static_cast<int32_t>(byte) << " ";
+    }
+    cout << endl;
 }
